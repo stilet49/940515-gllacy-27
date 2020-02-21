fb99e3bb232092e0ca2b0674336d3ceaae71cb5d
stilet49 <35362145+stilet49@users.noreply.github.com>
2020-02-21 12:03:48 +0300

    Оживил форму обратной связи
    Оживил форму обратной связи в  соответствии с заданием "21. Личный проект: оживляем интерактивные элементы"

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var link = document.querySelector(".callback-form-button");
+var popup = document.querySelector(".modal-callback");
+var mask = document.querySelector(".modal-mask");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var name_user = popup.querySelector("[name=callback-name]");
+var email = popup.querySelector("[name=callback-email]");
+var message = popup.querySelector("[name=callnack-message]");
+
+var storageName = localStorage.getItem("name");
+var storageEmail = localStorage.getItem("email");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("show-modal");
+  mask.classList.add("visible");
+
+  if (storageName && storageEmail) {
+    name_user.value = storageName;
+    email.value = storageEmail;
+    message.focus();
+  }
+  else {
+    name_user.focus();
+  }
+
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("show-modal");
+  mask.classList.remove("visible");
+  popup.classList.remove("error-modal");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name_user.value || !email.value || !message.value) {
+    evt.preventDefault();
+    popup.classList.remove("error-modal");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("error-modal");
+  }
+  else {
+    localStorage.setItem("name", name_user.value);
+    localStorage.setItem("email", email.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+  if (popup.classList.contains("show-modal")) {
+    popup.classList.remove("show-modal");
+    popup.classList.remove("error-modal");
+    mask.classList.remove("visible");
+  }
+}
+});
